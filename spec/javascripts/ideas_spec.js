954a21a8cc0e4ad3dd6257d9b6c5bf7526170568
Kristina Brown <kristina.frey.frey@gmail.com>
2015-07-07 21:39:51 -0600

    javascript tests added

---
+++ spec/javascripts/ideas_spec.js
@@ -0,0 +1,49 @@
+//= require chai
+//= require ideas
+var assert = chai.assert;
+
+describe('idea functions', function() {
+  
+  it("createCard is a fucntion", function() {
+    var idea = { 
+      id: 0, 
+      title: "title",
+      body: "body",
+      quality: 0
+    };
+    assert.ok(createCard(idea), "createCard is a function");
+  });
+    
+});
+
+describe('idea ajax', function(){
+  beforeEach(function () {
+    sinon.spy($, 'ajax');
+    this.requests = [];
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    this.xhr.onCreate = function (req) { this.requests.push(req); }.bind(this);
+  });
+  
+  afterEach(function () {
+    $.ajax.restore();
+    this.xhr.restore();
+  });
+  
+  it('deleteIdea makes an AJAX call to the server', function () {
+    deleteIdea();
+    assert($.ajax.calledOnce, 'We hit JQuery\'s AJAX method');
+    assert.strictEqual(this.requests.length, 1, 'We made one AJAX request');
+  });
+  
+  it('down makes an AJAX call to the server', function () {
+    down();
+    assert($.ajax.calledOnce, 'We hit JQuery\'s AJAX method');
+    assert.strictEqual(this.requests.length, 1, 'We made one AJAX request');
+  });
+  
+  it('up makes an AJAX call to the server', function () {
+    up();
+    assert($.ajax.calledOnce, 'We hit JQuery\'s AJAX method');
+    assert.strictEqual(this.requests.length, 1, 'We made one AJAX request');
+  });
+});
